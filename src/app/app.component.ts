fc8a488a685ac87a4a8aa7e946116a3293bb5b76
Mauro Mattos <mauromattos00@gmail.com>
2023-03-21 19:58:23 -0300

    feat: integrate advice service to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AdviceService } from './services/advice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'advice-generator';
+export class AppComponent implements OnInit {
+  adviceId$: Observable<number> = this.adviceService.adviceId$;
+  adviceText$: Observable<string> = this.adviceService.adviceText$;
+
+  constructor(private adviceService: AdviceService) { }
+
+  ngOnInit() {
+    this.adviceService.getRandomAdvice();
+  }
+  
+  getRandomAdvice() {
+    this.adviceService.getRandomAdvice();
+  }
 }
